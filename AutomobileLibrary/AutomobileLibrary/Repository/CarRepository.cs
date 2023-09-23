3e82079d8ba0cfa606dff5645e584daf44c28c73
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-09-23 20:19:15 +0700

    Store project

---
+++ AutomobileLibrary/AutomobileLibrary/Repository/CarRepository.cs
@@ -0,0 +1,21 @@
+﻿using AutomobileLibrary.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileLibrary.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        public Car GetCarById(int carID)
+        {
+            return CarManagement.Instance.GetCarByID(carID);
+        }
+        public IEnumerable<Car> GetCars() => CarManagement.Instance.GetCarList();
+        public void InsertCar(Car car) => CarManagement.Instance.AddNew(car);
+        public void DeleteCar(Car car) => CarManagement.Instance.Remove(car);
+        public void UpdateCar(Car car) => CarManagement.Instance.Update(car);
+    }
+}
